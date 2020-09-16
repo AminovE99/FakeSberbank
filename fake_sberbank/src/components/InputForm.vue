a54d67c8f10b566e9073775c451baad6379d7cab
emil <amionv99@gmail.com>
2020-09-16 16:50:03 +0300

    :sparkles: Create template for generating case
    Signed-off-by: emil <amionv99@gmail.com>

--- fake_sberbank/src/components/InputForm.vue
+++ fake_sberbank/src/components/InputForm.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
-    <form id="demo">
+    <form id="demo" @submit.prevent="onSubmit">
       <p>
         <label for="money">Количество переведенных денег: </label>
-        <input id="money" type="number">
+        <input id="money" v-model="money">
       </p>
       <p>
-        <label for="last_name">Фамилия</label>
-        <input id="last_name">
+        <label for="last_name">Первая буква фамилии</label>
+        <input id="last_name" v-model="last_name">
         <label for="first_name">Имя</label>
-        <input id="first_name">
+        <input id="first_name" v-model="first_name">
         <label for="middle_name">Отчество</label>
-        <input id="middle_name">
+        <input id="middle_name" v-model="middle_name">
       </p>
+      <button type="submit">Generate</button>
     </form>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      money: '',
+      first_name: '',
+      middle_name: '',
+      last_name: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.money)
+      console.log(this.first_name)
+    }
+  }
+}
+</script>
